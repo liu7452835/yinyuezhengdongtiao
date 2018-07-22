7620b8ab75a712b9c365257ac76afa9559d32fda
刘天宇 <lty@>
2018-07-22 15:35:47 +0800

    Initial Commit

---
+++ yinyuezhengdongtiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yinyuezhengdongtiao
+//
+//  Created by 刘天宇 on 2018/7/22.
+//  Copyright © 2018年 lty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yinyuezhengdongtiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yinyuezhengdongtiao
+//
+//  Created by 刘天宇 on 2018/7/22.
+//  Copyright © 2018年 lty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
