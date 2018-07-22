f73475be9746155194fbf00d0959a3c496e66b57
刘天宇 <lty@>
2018-07-22 15:40:32 +0800

    音乐振动条

--- yinyuezhengdongtiao/ViewController.m
+++ yinyuezhengdongtiao/ViewController.m
@@ -17,6 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    CAReplicatorLayer *replicatorLayer = [[CAReplicatorLayer alloc] init];
+    replicatorLayer.frame = CGRectMake(0, 100, 375, 400);
+    replicatorLayer.instanceCount = 5;
+    replicatorLayer.instanceTransform  = CATransform3DMakeTranslation(20, 0, 0);
+    replicatorLayer.instanceDelay = 0.2;
+    replicatorLayer.masksToBounds = YES;
+    replicatorLayer.backgroundColor = [UIColor blackColor].CGColor;
+    
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(14, 200, 10, 100);
+    layer.backgroundColor = [UIColor redColor].CGColor;
+    [replicatorLayer addSublayer:layer];
+    [self.view.layer addSublayer:replicatorLayer];
+    
+    layer.anchorPoint = CGPointMake(0, 1);
+    
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.duration = 0.5;
+    /*  animation.keyPath = @"position.y";
+     
+     animation.fromValue = @200;
+     animation.toValue = @150;
+     */
+    animation.keyPath = @"transform.scale.y";
+    animation.toValue = @0.1;
+    
+    
+    animation.autoreverses = YES;
+    animation.repeatCount = MAXFLOAT;
+    [layer addAnimation:animation forKey:nil];
+    
 }
 
 
